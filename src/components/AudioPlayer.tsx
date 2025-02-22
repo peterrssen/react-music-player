0993acce2422b8a7d92b00ba864be4566d3d411e
Your Name <you@example.com>
2025-02-22 10:36:21 +0100

    update

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -14,6 +14,10 @@ const AudioPlayer = () => {
     const [isExploding, setIsExploding] = useState(false);
 
     useEffect(() => {
+        setIsExploding(true)
+    }, [])
+
+    useEffect(() => {
         if (isPlaying) {
             playSong();
         }
