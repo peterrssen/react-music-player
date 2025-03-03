c9c5a6c11b3ebabe4db0ac93383edf7a6ba39592
Your Name <you@example.com>
2025-03-03 09:49:21 +0100

    new songs

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -4,6 +4,9 @@ import code from '../assets/Code.mp3';
 import werkstatt from '../assets/Werkstatt.mp3';
 import werkstattRemix from '../assets/Werkstatt-Remix.mp3';
 import schwammDrueberSong from '../assets/SchwammDrueberSong.mp3';
+import filsenOhFilsen from '../assets/filsenOhFilsen.mp3';
+import mitDemSchoenstenWagen from '../assets/mitDemSchoenstenWagen.mp3';
+import dieWinkelbauer from '../assets/dieWinkelbauer.mp3';
 
 import karnevalImMittelrheintalCover from '../assets/KarnevalImMittelrheintal-cover.jpg';
 import theSoundOfLoudnessCover from '../assets/TheSoundOfLoudness-cover.jpeg';
@@ -11,6 +14,9 @@ import codeCover from '../assets/code-cover.jpeg';
 import werkstattCover from '../assets/werkstatt-cover.jpeg';
 import werkstattRemixCover from '../assets/Werkstatt-Remix.jpeg';
 import schwammDrueberSongCover from '../assets/SchwammDrueberSong-cover.jpg';
+import filsenOhFilsenCover from '../assets/filsenOhFilsenCover.jpg';
+import mitDemSchoenstenWagenCover from '../assets/mitDemSchoenstenWagenCover.jpg';
+import dieWinkelbauerCover from '../assets/dieWinkelbauerCover.jpg';
 
 
 export interface Track {
@@ -65,6 +71,30 @@ export const tracks: Track[] = [
     },
     {
         id: 5,
+        title: 'Filsen, Oh Filsen',
+        url: filsenOhFilsen,
+        interpret: 'Ritter Rost',
+        cover: filsenOhFilsenCover,
+        duration: 214752,
+    },
+    {
+        id: 6,
+        title: 'Mit dem schönsten Wagen',
+        url: mitDemSchoenstenWagen,
+        interpret: 'De Wachportstürmer',
+        cover: mitDemSchoenstenWagenCover,
+        duration: 172080,
+    },
+    {
+        id: 7,
+        title: 'Die Winkelbauer',
+        url: dieWinkelbauer,
+        interpret: 'punAIsher',
+        cover: dieWinkelbauerCover,
+        duration: 175032,
+    },
+    {
+        id: 8,
         title: 'Werkstatt Remix',
         url: werkstattRemix,
         interpret: 'DJ Glitch feat, Reif R. Schaid',
